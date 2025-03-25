e548e73cbbdc6ef5066fe1dbd633351cb9273e4e
Evgeny Korytny <37796029+Korytny@users.noreply.github.com>
2025-03-25 19:09:43 +0300

    Удаление всех файлов (подготовка к чистой заливке)

--- src/types/pages/projects.ts
+++
@@ -1,9 +0,0 @@
-export interface ProjectCardDetailsProps {
-  images: string[];
-  title: string;
-  description: string;
-  features: string[];
-  technologies: string[];
-  icon: React.ReactNode;
-  index: number;
-}
