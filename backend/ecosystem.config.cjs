5b272e5081cb163325c0d55441c1fb71cebbaba6
Evgeny Korytny <37796029+Korytny@users.noreply.github.com>
2025-06-22 10:09:58 +0300

    https

--- backend/ecosystem.config.cjs
+++ backend/ecosystem.config.cjs
@@ -8,15 +8,15 @@ module.exports = {
       NODE_ENV: "development",
       PORT: 8002,
       HOST: "0.0.0.0",
-      SERVER_URL: "http://103.82.249.157:8002",
-      ALLOWED_ORIGINS: "http://103.82.249.157,http://localhost:3000"
+      SERVER_URL: "https://103.82.249.157:8002",
+      ALLOWED_ORIGINS: "https://103.82.249.157,https://localhost:3000"
     },
     env_production: {
       NODE_ENV: "production",
       PORT: 8002,
       HOST: "0.0.0.0",
-      SERVER_URL: "http://103.82.249.157:8001",
-      ALLOWED_ORIGINS: "http://103.82.249.157"
+      SERVER_URL: "https://103.82.249.157:8001",
+      ALLOWED_ORIGINS: "https://103.82.249.157"
     }
   }]
 }
